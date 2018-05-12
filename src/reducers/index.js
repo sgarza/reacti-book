5e996e35c8e8476b03e90c13a98f2a85b3fc9ca1
Sergio de la Garza <sergio@delagarza.io>
2018-05-12 15:42:33 -0500

    Setup firebase reducers and store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+import { firebaseStateReducer as firebase } from "react-redux-firebase";
+import { reducer as formReducer } from "redux-form";
 import { routerReducer } from "react-router-redux";
 
 export default combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  firebase,
+  form: formReducer
 });
--- src/store.js
+++ src/store.js
@@ -1,15 +1,33 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { routerMiddleware } from "react-router-redux";
 import thunk from "redux-thunk";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import firebase from "firebase";
 import createHistory from "history/createBrowserHistory";
 import rootReducer from "./reducers";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import firebaseConfig from "./config/firebase";
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users"
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig)
+)(createStore);
 
 export const history = createHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [thunk, routerMiddleware(history)];
+const middleware = [
+  thunk.withExtraArgument(getFirebase),
+  routerMiddleware(history)
+];
 
 if (process.env.NODE_ENV === "development") {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
@@ -21,6 +39,10 @@ if (process.env.NODE_ENV === "development") {
 
 const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 
-const store = createStore(rootReducer, initialState, composedEnhancers);
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  composedEnhancers
+);
 
 export default store;
